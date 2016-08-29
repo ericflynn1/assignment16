03ae846f1b984ca55c9acd7692802984cc942af9
Eric <ericgflynn@gmail.com>
2016-08-29 10:06:15 -0400

    app4 in class

--- app4.js
+++ app4.js
@@ -1,3 +1,26 @@
-function celebrity (list) {
-    
+function celebrity(arrayOfCelebrities) {
+    let tally = [];
+    for (let i = 0; i < arrayOfCelebrities.length; i++) {
+        for (let j = arrayOfCelebrities[i].birth; j <= arrayOfCelebrities[i].death; j++ ) {
+            tally.push(j);
+        }        
+        
+    }
+    tally.sort();
+    console.log(tally);
+    let biggun = 0;
+    let current = 0;
+    let currentYear = tally[0];
+    for (let i = 0; i < tally.length; i++){
+        if (tally[i] === currentYear) {
+            current++;
+        } else { 
+            if (current > biggun) {
+                biggun = current;
+            }
+            current = 1;
+        }
+        currentYear = tally[i];
+    }
+    return biggun;
 }
