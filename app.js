61317b1bbe8dde2d46777a40c078ade7e419a4fa
Eric <ericgflynn@gmail.com>
2016-08-29 14:04:04 -0400

    combined apps

--- app.js
+++ app.js
@@ -19,3 +19,69 @@ function longx(words) {
 
 console.log(longx('xxrcxxxxh'));
 console.log('--------------------------------------------------');
+function longest(word) {
+    let x = word.split("");
+    let biggun = {};
+    for (let i = 0; i < x.length; i++) {
+        if (biggun.hasOwnProperty(x[i]) === false) {
+            biggun[x[i]] = 1
+        }
+        else {
+            biggun[x[i]] = biggun[x[i]] + 1;
+        }
+    }
+console.log(biggun);
+    let count = 0;
+    for (let thing in biggun) {
+        if (biggun[thing] > count) {
+            count = biggun[thing];
+        }
+    }
+    return count;
+}
+console.log(longest('xxxcxcxx'));
+
+console.log('---------------------------------------------------------')
+function reduce(num, den){
+        var newNum = newNum || num;
+        var newDen = newDen || den;
+        var i = 2;
+        while (i <= newNum && i <= newDen){
+            if (newNum%i === 0 && newDen%i === 0){
+            newNum = newNum/i;
+            newDen = newDen/i;
+        } else {
+            i++;
+        }
+    };
+    return newNum + ", " + newDen;
+}
+
+console.log(reduce(4,8));
+
+function celebrity(arrayOfCelebrities) {
+    let tally = [];
+    for (let i = 0; i < arrayOfCelebrities.length; i++) {
+        for (let j = arrayOfCelebrities[i].birth; j <= arrayOfCelebrities[i].death; j++ ) {
+            tally.push(j);
+        }        
+        
+    }
+    tally.sort();
+    console.log(tally);
+    let biggun = 0;
+    let current = 0;
+    let currentYear = tally[0];
+    for (let i = 0; i < tally.length; i++){
+        if (tally[i] === currentYear) {
+            current++;
+        } else { 
+            if (current > biggun) {
+                biggun = current;
+            }
+            current = 1;
+        }
+        currentYear = tally[i];
+    }
+    return biggun;
+}
